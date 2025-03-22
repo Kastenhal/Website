cc585bf57424b0ee06b043f75d06d39155a87b60
Kastenhal <theephereal@gmail.com>
2025-03-22 00:43:04 -0400

    feat: add new pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,93 @@
 "use client";
 import { Typewriter } from "react-simple-typewriter";
+import Link from "next/link";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [showSecondTypewriter, setShowSecondTypewriter] = useState(false);
+  const [hideFirstCursor, setHideFirstCursor] = useState(false);
+ 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSecondTypewriter(true);
+      setHideFirstCursor(true);
+    }, 3500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex flex-col font-[family-name:var(--font-geist-mono)]">
+        <div className="flex flex-col font-[family-name:var(--font-geist-mono)] gap-[10px]">
           <h1 className="text-xl font-semibold">
             <Typewriter
-              words={["Markus Smith", "Full Stack Developer"]}
-              loop={0} // Infinite loop
-              cursor
+              words={["Markus Smith"]}
+              cursor={!hideFirstCursor}
               cursorStyle="|"
               typeSpeed={50}
               deleteSpeed={50}
               delaySpeed={3000}
             />
           </h1>
+
+          {showSecondTypewriter && (
+            <h1 className="text-xl font-semibold transition-opacity duration-500 ease-in-out opacity-100 text-red-400">
+              <Typewriter
+                words={["Software Developer"]}
+                cursor
+                cursorStyle="|"
+                typeSpeed={50}
+                deleteSpeed={50}
+                delaySpeed={3000}
+              />
+            </h1>
+          )}
+
           <h2 className="text-base font-light">North Carolina, United States</h2>
+          <div className="flex flex-row gap-[12px] text-base font-light">
+            <Link
+              href="/blog"
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            >
+              <Image
+                className="dark:invert"
+                src="/globe.svg"
+                alt="Globe icon"
+                width={20}
+                height={20}
+              />
+              Blog
+            </Link>
+
+            <Link
+              href="/resume"
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            >
+              <Image
+                className="dark:invert"
+                src="/file.svg"
+                alt="File icon"
+                width={20}
+                height={20}
+              />
+              Résumé
+            </Link>
+
+            <Link
+              href="/projects"
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            >
+              <Image
+                className="dark:invert"
+                src="/window.svg"
+                alt="Window icon"
+                width={20}
+                height={20}
+              />
+              Projects
+            </Link>
+          </div>
         </div>
       </main>
 
