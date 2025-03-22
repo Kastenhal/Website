44b7506f3de9022423b7cf37047947aef81b1ab4
Kastenhal <theephereal@gmail.com>
2025-03-22 13:39:29 -0400

    fix: reduce typewriter effect timeout duration

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     const timer = setTimeout(() => {
       setShowSecondTypewriter(true);
       setHideFirstCursor(true);
-    }, 3500);
+    }, 2250);
     return () => clearTimeout(timer);
   }, []);
 
