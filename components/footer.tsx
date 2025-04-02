774b5f0466298488412860e2f30d610dbc8f317b
Kastenhal <kastenhaloverguard@protonmail.com>
2025-04-02 01:32:00 -0400

    fix: declare type for navigation link parameters

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image"
 
+interface ExternalNavigatorProps {
+    href: string,
+    icon: string
+}
+
 export function Footer({children}: {children: React.ReactNode}) {
     return(
         <div className="flex flex-row gap-8 grow-0 min-h-16">
@@ -8,7 +13,7 @@ export function Footer({children}: {children: React.ReactNode}) {
     );
 }
 
-export function ExternalNavigator({href, icon}) {
+export function ExternalNavigator({href, icon}: ExternalNavigatorProps) {
     return(
         <a href={href} target="_blank" rel="noopener noreferrer">
             <Image
